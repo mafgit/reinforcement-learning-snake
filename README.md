3e0a8ac5d27b270b9d1959448b23275ba4d3ada0
Abdullah <mohammadabdullahfarooqui27@gmail.com>
2026-04-07 15:06:52 +0500

    metadata & readme updated

--- README.md
+++ README.md
@@ -1,12 +1,26 @@
-# RL Snake
+# Reinforcement Learning Snake
 
-- Reinforcement Learning Based Model Training
+I used reinforcement learning (reward-based training) to train the snake to:
+
+1. decide actions itself,
+1. prioritize avoiding collisions (-30 reward),
+1. prioritize eating food (+15 reward),
+1. avoid wasting time (-1 reward for no eating).
+
+### Demo
+
+<video src="demo.mp4" width="100%" controls></video>
+
+### Made using
+
 - Next.js
-- React.memo & useCallback
-- Tailwind
 - TypeScript
+- Reinforcement learning & Bellman equation
+- Tailwind
+- memoization (to improve performance)
+- modular production-level folder structure
 
-# Issues
+### Issues
 
 - Food location in giving game state to model logic for wraparound
 - Snake should stop when eating
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,10 +8,35 @@ const poppins = Poppins({
 	weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
 });
 
+const description =
+	"Snake trained via reinforcement learning to make it avoid collisions and prioritize eating food without wasting time.";
+
 export const metadata: Metadata = {
-	title: "Snake Game (Reinforcement Learning)",
-	description:
-		"Used reinforcement learning to train snake to eat food and avoid collision in a snake game. Made in Next.js + TypeScript.",
+	title: "RLSnake",
+	description,
+	authors: {
+		name: "Mohammad Abdullah Farooqui",
+		url: "https://www.github.com/mafgit",
+	},
+	keywords: [
+		"reinforcement",
+		"learning",
+		"snake",
+		"reward",
+		"food",
+		"snake game",
+		"reinforcement learning",
+		"auto",
+		"ai snake",
+	],
+	applicationName: "Reinforcement Learning Snake",
+	openGraph: {
+		type: "website",
+		url: "https://rlsnake.vercel.app",
+		title: "RL Snake",
+		description,
+		siteName: "My Website",
+	},
 };
 
 export default function RootLayout({
